fa27d9bfa3fe6340a4002154b20a0acfbdb96f17
Bhuvan <bhaskarbhuvan2004@gmail.com>
2026-01-08 10:04:05 +0530

    add buyer domain

--- backend/buyerDomainService/routes/profile.js
+++ backend/buyerDomainService/routes/profile.js
@@ -1,5 +1,33 @@
-import prisma from "../config/redisClient.js"
+import prisma from "../config/prismaClient";
 
-const update_number = async()=>{
+const add_number = async (req) => {
+  const { authUserId, mobileNumber } = req.body;
 
-}
+  return await prisma.user.upsert({
+    where: {
+      authUserId
+    },
+    update: {
+      mobileNumber
+    },
+    create: {
+      authUserId,
+      mobileNumber
+    }
+  });
+};
+
+const update_number = async (req) => {
+  const { authUserId, mobileNumber } = req.body;
+
+  return await prisma.user.update({
+    where: {
+      authUserId
+    },
+    data: {
+      mobileNumber
+    }
+  });
+};
+
+export { add_number, update_number };
