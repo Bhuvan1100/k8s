aad8cb611962ed0f975f86ca6a6ea41829b0e961
Bhuvan <bhaskarbhuvan2004@gmail.com>
2026-05-19 02:28:42 +0530

    final commit

--- backend/apiGateWay/src/index.js
+++ backend/apiGateWay/src/index.js
@@ -56,7 +56,7 @@ import { fetchProposals } from "./router/pricing/fetchProposal.js"
 const app = express()
 const port = 4000
 
-app.get("/health", (req, res) => {
+app.get("/api/health", (req, res) => {
   res.status(200).json({
     status: "OK",
     service: "API_GATEWAY"
